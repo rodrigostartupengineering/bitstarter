6866fa046add081f6f8f2d6a2bfd72480bbed28a
rodrigostartupengineering <rodrigo.startup.engineering@gmail.com>
2013-07-09 17:34:10 +0000

    Changed web.js

--- web.js
+++ web.js
@@ -3,7 +3,16 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+    var fs = require('fs');
+    var bData = new Buffer(256);
+
+    fs.readFile('index.html', function(err, data){
+	if(err) throw err;
+	bData.write(data, "utf-8");
+    });	
+
+    response.send(bData.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
